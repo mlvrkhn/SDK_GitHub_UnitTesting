3bf738fcd1bbb499524c703d3e0b6666fa067b89
Martin Gawlyta <martin.guide@gmail.com>
2020-10-27 02:01:16 +0100

    Add tests logic

--- GitHubSDK.test.js
+++ GitHubSDK.test.js
@@ -2,16 +2,32 @@ import GitHubSDK from './GitHubSDK';
 import { expect } from '@jest/globals';
 
 
-describe('GitHubSDK class exitence', () => {
-    it('checks if the instance of a GitHubSDK class is created', () => {
-        const api = new GitHubSDK('Name', 'Lastname');
-        // console.dir(api)
-        expect(api).toBeInstanceOf(GitHubSDK);
-    });
+describe('tests for GitHubSDK', () => {
+    describe('GitHubSDK class exitence'() => {
 
-    it('gets user data and ', async () => {
-        const api = new GitHubSDK('https://api.github.com/users/');
-        const respond = await api.getUserData();
-        expect(respond).toBeInstanceOf(GitHubSDK);
-    });
+        it('checks if the instance of a GitHubSDK class is created', () => {
+            const api = new GitHubSDK('Name', 'Lastname');
+            console.dir(api)
+            expect(api).toBeInstanceOf(GitHubSDK);
+        });
+
+        it('gets user data and ', asyc() => {
+            const api = new GitHubSDK('Martin', 'Gawlyta');
+            const respond = await api.getUserData();
+            expect(respond).toBeInstanceOf(GitHubSDK);
+        });
+
+    })
+    describe('function getUserData()', () => {
+        it('returns user data and converts into txt', () => {
+
+        })
+        it('throws error if invalid token passed', () => {
+
+        })
+        it('throws error if invalid token passed', () => {
+
+        })
+    })
+    
 });
