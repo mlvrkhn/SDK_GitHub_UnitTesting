ccc3f7fe16c1b167aa995af3330bd9a8459155be
Martin Gawlyta <martin.guide@gmail.com>
2020-11-20 20:27:13 +0100

    Divide updateRepositoriesView() onto 3 separate functions

--- src/GitHubSDK.js
+++ src/GitHubSDK.js
@@ -23,7 +23,6 @@ export default class GitHubSDK {
             };
             return this._fetch(apiURL, options);
     };
-
     getPublicRepos(userToCheck = this.login, nrOfRepos = 4) {
         if (typeof userToCheck !== 'string' || typeof nrOfRepos !== 'number') {
             if (nrOfRepos < 1) {
@@ -45,7 +44,6 @@ export default class GitHubSDK {
             return this._fetch(url, options);
         }
     };
-
     toggleRepoPrivacy(repoName, ifPrivate) {
         if (typeof repoName !== 'string' || typeof ifPrivate !== 'boolean') {
             throw new Error('Invalid arguments passed');
@@ -89,7 +87,6 @@ export default class GitHubSDK {
             }
         }
     }
-
     deleteRepo(repoName) {
         const url = this._getURL(false, 'repos', this.login, repoName);
         const options = {
@@ -101,36 +98,31 @@ export default class GitHubSDK {
         }
         return this._fetch(url, options);
     }
-
-    updateRepositoriesView(data) {
+    updateRepositoriesView(repositories) {
         const protoSelector = 'repo__container-proto'
         const repoElement = document.querySelector('.' + protoSelector);
+        const parent = repoElement.parentNode;
 
-        data.forEach(repo => {
-            const {name, created_at, updated_at, html_url, description} = repo;        
-            const newElement = repoElement.cloneNode(true);
-            newElement.classList.remove(protoSelector)
-
-            const created = created_at.slice(0, 10);
-            const updated = updated_at.slice(0, 10);
-            const repoTitle = newElement.querySelector('.repo__title');        
-            const repoHTML = newElement.querySelector('.repo__html');
-            const repoCreatedAt = newElement.querySelector('.repo__created-at');        
-            const repoUpdatedAt = newElement.querySelector('.repo__updated-at');        
-            const repoDescription = newElement.querySelector('.repo__description');        
-            const parent = repoElement.parentNode;
-
-            repoTitle.textContent = `Name: ${name}`;
-            repoHTML.textContent = `${html_url}`;
-            repoHTML.setAttribute('href', html_url)
-            repoCreatedAt.textContent = `Created at: ${created}`;
-            repoUpdatedAt.textContent = `Updated at: ${updated}`;
-            repoDescription.textContent = description;
-            
-            parent.appendChild(newElement);
+        repositories.forEach(repo => {
+            const repoDOMElement = this._createRepoContainer(repoElement);
+            this._populateRepoData(repoDOMElement, repo, parent)
         });
     }
+    _populateRepoData(element, data, root) {
+        const {name, created_at, updated_at, html_url, description} = data;
 
+        element.classList.remove('repo__container-proto');
+        element.querySelector('.repo__title').textContent = `Name: ${name}`;        
+        element.querySelector('.repo__html').textContent = `${html_url}`;
+        element.querySelector('.repo__created-at').textContent = `Created at: ${created_at.slice(0, 10)}`;        
+        element.querySelector('.repo__updated-at').textContent = `Updated at: ${updated_at.slice(0, 10)}`;        
+        element.querySelector('.repo__description').textContent = description;
+        
+        root.appendChild(element);
+    }
+    _createRepoContainer(proto) {
+        return proto.cloneNode(true);
+    }
     _checkIfValidData(url, login, token) {
         if (url === undefined || login === undefined || token == undefined || typeof token !== 'string') {
             throw new Error('Missing parameters in instance creation')
@@ -138,7 +130,6 @@ export default class GitHubSDK {
             return;
         }
     }
-
     _getURL(ifcors = false, prefix, prop, val = '') {
         let apiURL = path.join(prefix, prop, val);
 
@@ -148,7 +139,6 @@ export default class GitHubSDK {
             return this.cors + this.url + apiURL;
         }
     };
-
     _fetch(url, options) {
         return fetch(url, options)
             .then(res => (!res.ok) ? Promise.reject(res) : res.json());
